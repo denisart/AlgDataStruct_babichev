fbe24887257af5543f79f873a27f2915f18d4420
Denis Artyushin <artyushin@healbe.com>
2020-06-04 00:19:44 +0300

    add problem 5

---
+++ problems/problem5.cpp
@@ -0,0 +1,87 @@
+/*
+    Problem 5. Two heaps
+
+    Input file:         std::cin
+    Output file:        std::cout
+    Time complexity:    2 sec
+    Memory complexity:  64 Mb
+*/
+#include <bits/stdc++.h>
+
+int findMin(const std::vector<int>& w, int sum) {
+    /*
+        Let A = {A1, A2, ..., An} be a set with positive
+        integer numbers;
+
+        Let d[i][j] = 1, if some subset B in {A1, A2, ..., Ai}
+                         has sum equals j,
+                      0, owherwise
+        0 <= i <= n, 0 <= j <= S, where S = Sum(A) := A1 + A2 + ... + An.
+
+        Note that
+            d[i][j] = d[i - 1][j] or
+                      d[i - 1][j - Ai]
+
+        If S1 and S2 be partition of A, then
+            Sum(S1) = j and Sum(S2) = S - j.
+
+        We find
+            Min{S - 2 * j}, where 0 <= j <= S / 2 and
+                                  d[n][j] = 1.
+    */
+    bool d[w.size() + 1][sum + 1];
+
+    // d[i][0] = 1, because
+    // 0 = S(empty set)
+    for (int i = 0; i <= w.size(); i++) {
+        d[i][0] = true;
+    }
+
+    // d[0][j] = 0, because
+    // S(empty set) = 0 :)
+    for (int j = 1; j <= sum; j++) {
+        d[0][j] = false;
+    }
+
+    // using recurrence relation
+    // d[i][j] = d[i - 1][j] or d[i - 1][j - Ai]
+    for (int i = 1; i <= w.size(); i++) {
+        for (int j = 1; j <= sum; j++) {
+            d[i][j] = d[i - 1][j];
+
+            if (w[i - 1] <= j) {
+                d[i][j] |= d[i - 1][j - w[i - 1]];
+            }
+        }
+    }
+
+    int diff = INT_MAX;
+
+    // S - 2 * j is minimal when
+    // j is maximal
+    for (int j = sum / 2; j >= 0; j--) {
+        if (d[w.size()][j]) {
+            diff = sum - 2 * j;
+            break;
+        }
+    }
+
+    return diff;
+};
+
+
+int main() {
+    short N;
+    std::cin >> N;
+
+    std::vector<int> weights(N);
+    int sum = 0;
+    for (int i = 0; i < N; i++) {
+        std::cin >> weights[i];
+        sum += weights[i];
+    }
+
+    std::cout << findMin(weights, sum) << std::endl;
+
+    return 0;
+}
