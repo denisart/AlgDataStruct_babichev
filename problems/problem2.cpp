f0ee798abb43e9882f9e92ee85965351a48b9dfe
Denis Artyushin <artyushin@healbe.com>
2020-06-04 12:43:08 +0300

    update comments in problem 2

--- problems/problem2.cpp
+++ problems/problem2.cpp
@@ -5,6 +5,27 @@
     Output file:        std::cout
     Time complexity:    2 sec
     Memory complexity:  64 Mb
+
+    Let A = (a1, a2, ..., aNa) and B = (b1, b2, ..., bNa)
+    are sequences. Let ai <= aj and bi <= bj for any i, j.
+    Find the number of common elements in A and B. For
+    example, A = (0, 0, 1, 1, 2, 3) and B = (0, 1, 1, 2). Then
+    the common elements are (0, 1, 1, 2) and their number
+    equals 4.
+
+    Input:
+        Na Nb
+        a1
+        a2
+        ...
+        aNa
+        b1
+        b2
+        ...
+        bNb
+
+    Output:
+        The number of common elements
 */
 #include <bits/stdc++.h>
 
