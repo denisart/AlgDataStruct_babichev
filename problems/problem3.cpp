31528b062562502abbd36ffd9838dd9166831c39
Denis Artyushin <artyushin@healbe.com>
2020-06-04 12:52:57 +0300

    update comments in problem 3

--- problems/problem3.cpp
+++ problems/problem3.cpp
@@ -5,6 +5,30 @@
     Output file:        std::cout
     Time complexity:    1 sec
     Memory complexity:  16 Mb
+
+    Let
+        p(x) = a_N x^N + a_{N-1} x^{N-1} + ... + a_1 x + a_0,
+        where a_i is a integer number and 2 <= N <= 100000.
+
+    Find p(x_i) (mod K) for 0 <= x_i <= 10^9 and 1 <= i <= M, where
+    1 <= M <= 10000 and for 10 <= K <= 10^9.
+
+    Input:
+        N M K
+        a_N
+        a_{N-1}
+        ...
+        a_0
+        x_1
+        x_2
+        ...
+        x_{M}
+
+    Output:
+        p(x_1) (mod K)
+        p(x_2) (mod K)
+        ...
+        p(x_M) (mod K)
 */
 #include <bits/stdc++.h>
 
@@ -27,6 +51,7 @@ int main() {
         pow = 1;
         std::cin >> x;
 
+        // x^n (mod K) = x * (x^{n-1} (mod K))
         for (int i = 0; i < N + 1; i++) {
             poly_value = (poly_value + coefs[i] * pow) % MOD;
             pow = (pow * x) % MOD;
